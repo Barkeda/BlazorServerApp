212afe43be471eee38bd657132e8d990e58a936d
LAPTOP-T1J5NJSM\Ejer <filipjessen@gmail.com>
2022-03-24 09:58:05 +0100

    Add project files.

---
+++ BlazorServerApp/Data/FileHandlerController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BlazorServerApp.Data
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileHandlerController : ControllerBase
+    {
+        // GET: api/<FileHandlerController>
+        [HttpGet]
+        public string Get()
+        {
+            return System.IO.File.ReadAllText(@"C:\test\Test.txt");
+        }
+    }
+}
